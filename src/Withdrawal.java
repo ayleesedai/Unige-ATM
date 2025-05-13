ff6543bf0f27370d1fc8645d408050d0ce00b5a9
ayleesedai <andrea.briozzo@gmail.com>
2025-05-13 10:54:42 +0200

    Update BL

--- src/Withdrawal.java
+++ src/Withdrawal.java
@@ -117,9 +117,10 @@ public class Withdrawal extends Transaction
             case 3: // corresponding amount from amounts array
             case 4:
             case 5:
-               userChoice = amounts[ input ]; // save user's choice
+               // userChoice = amounts[ input ]; // save user's choice
                screen.displayMessageLine( "How much do you want to withdraw?");
-
+               int customAmount = keypad.getInput(); // get user input through keypad
+               userChoice = customAmount; // save user's choice
                break;       
             case CANCELED: // the user chose to cancel
                userChoice = CANCELED; // save user's choice
